cfc50155bb3b46d25bf66a378684ad88b916d9df
ipb933 <idoperlman9@gmail.com>
2026-02-03 12:54:18 +0200

    robot

---
+++ app/src/main/java/com/example/android_project/Robot.java
@@ -0,0 +1,37 @@
+package com.example.android_project;
+
+public class Robot {
+    String teamName;
+    int teamNumber;
+    double averageScore;
+
+    public Robot(String teamName, int teamNumber, double averageScore) {
+        this.teamName = teamName;
+        this.teamNumber = teamNumber;
+        this.averageScore = averageScore;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getTeamNumber() {
+        return teamNumber;
+    }
+
+    public void setTeamNumber(int teamNumber) {
+        this.teamNumber = teamNumber;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
+}
